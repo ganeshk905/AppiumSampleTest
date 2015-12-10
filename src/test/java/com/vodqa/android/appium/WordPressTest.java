b4074093028a576dd6c38b79e01affc4257a6360
Sai Krishna <saikrishna321@yahoo.com>
2015-12-10 15:29:04 +0530

    reverted code

--- src/test/java/com/vodqa/android/appium/WordPressTest.java
+++ src/test/java/com/vodqa/android/appium/WordPressTest.java
@@ -1,16 +1,22 @@
 package com.vodqa.android.appium;
 
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.vodqa.utils.BaseTest;
-
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidKeyCode;
+import io.appium.java_client.remote.MobileCapabilityType;
 
-public class WordPressTest extends BaseTest {
+public class WordPressTest  {
 
 	String userName = "org.wordpress.android:id/nux_username";
 	String password = "org.wordpress.android:id/nux_password";
@@ -19,13 +25,39 @@ public class WordPressTest extends BaseTest {
 	String postTitle = "org.wordpress.android:id/post_title";
 	String postContent = "org.wordpress.android:id/post_content";
 	String savePost = "org.wordpress.android:id/menu_save_post";
+	
+	public AppiumDriver<MobileElement> driver;
+	public WebDriverWait wait;
+	
+	@Before
+	public void setUp() throws Exception {
+			// set up appium for android instance
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability("deviceName", "Nexus 5");
+			capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "org.wordpress.android");
+			capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "org.wordpress.android.ui.WPLaunchActivity");
+			capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/build/wordpress.apk");
+			driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			wait = new WebDriverWait(driver, 30);
+		}
+
+
+	
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
 
 	@Test
 	public void logIn() throws InterruptedException {
+		// Login to the Wordpress Application
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.id(userName)));
 		driver.findElementById(userName).sendKeys("vodqa@gmail.com");
 		driver.findElementById(password).sendKeys("Hello12345678");
 		driver.findElementById(signIn).click();
+		
+		//Wait for the new post button and add a post
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.id(newPost)));
 		driver.findElementById(newPost).click();
 		driver.findElementById(postTitle).sendKeys("AppiumTest");
--- src/test/java/com/vodqa/ios/appium/RecipesTest.java
+++ src/test/java/com/vodqa/ios/appium/RecipesTest.java
@@ -1,16 +1,44 @@
 package com.vodqa.ios.appium;
 
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.vodqa.utils.BaseTest;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
-public class RecipesTest extends BaseTest {
+public class RecipesTest{
 
 	String addRecipe = "Add";
 	String receipeName = "Recipe Name";
 	String saveRecipe = "//UIAApplication[1]/UIAWindow[2]/UIANavigationBar[1]/UIAButton[3]";
+
+	public AppiumDriver<MobileElement> driver;
+	public WebDriverWait wait;
+
+	@Before
+	public void setUp() throws Exception {
+		// set up appium for android instance
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
+		capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/build/Recipes.app");
+		driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		wait = new WebDriverWait(driver, 30);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
 
 	@Test
 	public void addRecipe() throws InterruptedException {
@@ -28,6 +56,6 @@ public class RecipesTest extends BaseTest {
 
 		// navigate to home page
 		driver.findElementByAccessibilityId("Recipes").click();
-		
+
 	}
 }
